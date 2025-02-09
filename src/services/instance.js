909b9d7ad27b4cc335a61c6114956f23d678930c
PriyaTamz <vishnupriyatk1996@gmail.com>
2025-02-09 20:45:45 +0530

    re submission

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,25 +1,31 @@
 import axios from "axios";
 
-const baseurl = 'https://backend-lms-ftog.onrender.com';
-//const baseurl = 'http://localhost:3001';
+// Determine base URL dynamically based on the environment
+const baseurl =
+    process.env.NODE_ENV === "production"
+        ? "https://backend-lms-ftog.onrender.com" // Production backend
+        : "http://localhost:3001"; // Local backend
 
 const instance = axios.create({
     baseURL: baseurl,
     //timeout: 60000,
     headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
     },
-    withCredentials: true
-})
-
-instance.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-}, (error) => {
-    return Promise.reject(error);
+    withCredentials: true,
 });
+
+instance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default instance;
